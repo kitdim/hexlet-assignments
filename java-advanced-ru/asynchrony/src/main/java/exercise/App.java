e9797b149c079ff3d1099b3693ce7189bf6da0ce
@hexlet/cli <support@hexlet.io>
2024-04-06 21:43:21 +0300

    submit java-advanced-ru/asynchrony

--- java-advanced-ru/asynchrony/src/main/java/exercise/App.java
+++ java-advanced-ru/asynchrony/src/main/java/exercise/App.java
@@ -16,28 +16,44 @@ class App {
     public static CompletableFuture<String> unionFiles(String file1,
                                                        String file2,
                                                        String file3) {
-        return CompletableFuture.supplyAsync(() -> {
+        CompletableFuture<String> futureReadFirstFile = CompletableFuture.supplyAsync(() -> {
             try {
-                String content1 = new String(Files.readAllBytes(Paths.get(file1)));
-                String content2 = new String(Files.readAllBytes(Paths.get(file2)));
-
-                String combinedContent = content1 + content2;
-
-                Files.write(Paths.get(file3), combinedContent.getBytes(StandardCharsets.UTF_8));
-
-                return "Files successfully combined and written to destination file.";
+                var path = Path.of(file1);
+                return Files.readString(path);
             } catch (IOException e) {
-                return "Error combining files: " + e.getMessage();
+                throw new RuntimeException(e);
             }
         });
+
+        CompletableFuture<String> futureReadSecondFile = CompletableFuture.supplyAsync(() -> {
+            try {
+                var path = Path.of(file2);
+                return Files.readString(path);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        return futureReadFirstFile.thenCombine(
+                futureReadSecondFile, (firstValue, secondValue) -> {
+                    try {
+                        var resultPath = Path.of(file3);
+                        var resultValue = firstValue + " " + secondValue;
+                        Files.writeString(resultPath, resultValue);
+                        return resultValue;
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }).exceptionally(ex -> {
+            System.out.println(ex.getMessage());
+            return ex.getMessage();
+        });
     }
-    // END
+     // END
 
     public static void main(String[] args) throws Exception {
         // BEGIN
-        CompletableFuture<String> result = unionFiles("src/main/resources/file1.txt", "src/main/resources/file2.txt", "src/main/resources/dest.txt");
 
-        result.thenAccept(System.out::println);
         // END
     }
 }
